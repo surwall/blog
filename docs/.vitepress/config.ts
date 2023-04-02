5a165fb268064915484fa8e9bca5ce89f8a7435d
Marcus Xu <surwall07@gmail.com>
2023-04-02 20:30:00 +0800

    chore: configure workflow

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/',
   title: '前端学习',
   description: 'stay awesome',
   themeConfig: {
