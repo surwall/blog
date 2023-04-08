2e18d0e4d44e0013221ee1b9e4ce6ff89a5fd19e
Marcus Xu <surwall07@gmail.com>
2023-04-08 20:52:21 +0800

    chore: 添加pwa支持

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vitepress'
+import { withPwa } from '@vite-pwa/vitepress'
 
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
+export default withPwa(defineConfig({
   lang: 'zh-CN',
   base: '/',
   lastUpdated: true,
@@ -10,6 +11,74 @@ export default defineConfig({
   /* markdown 配置 */
   markdown: {
     lineNumbers: true,
+  },
+  pwa: {
+    strategies: 'generateSW',
+    registerType: 'prompt',
+    manifest: {
+      display: 'standalone',
+      name: "surwall's blog",
+      short_name: "surwall",
+      theme_color: "#bd34fe",
+      icons: [
+    {
+      "src": "/icons/icon-72x72.png",
+      "sizes": "72x72",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-96x96.png",
+      "sizes": "96x96",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-128x128.png",
+      "sizes": "128x128",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-144x144.png",
+      "sizes": "144x144",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-152x152.png",
+      "sizes": "152x152",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-384x384.png",
+      "sizes": "384x384",
+      "type": "image/png",
+      "purpose": "maskable any"
+    },
+    {
+      "src": "/icons/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable any"
+    }
+      ]
+    },
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true,
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    },
+    devOptions: {
+      enabled: true
+    }
   },
   head: [
     [
@@ -71,3 +140,4 @@ export default defineConfig({
     socialLinks: [{ icon: 'github', link: 'https://github.com/surwall' }],
   },
 })
+)
